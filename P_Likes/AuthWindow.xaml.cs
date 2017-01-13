7a2d48bfe4cf8fd9a5646deb536c7a75c4783d93
Андрій Токарський < >
2017-01-13 17:59:17 +0200

    first commit

---
+++ P_Likes/AuthWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace P_Likes
+{
+    /// <summary>
+    /// Interaction logic for AuthWindow.xaml
+    /// </summary>
+    public partial class AuthWindow : Window
+    {
+        private String userToken;
+        private String userId;
+        const String appId = "5072484";
+
+        public AuthWindow()
+        {
+            InitializeComponent();
+            
+            GetToken.LoadCompleted += GetToken_LoadCompleted;
+            GetToken.Navigate($@"https://oauth.vk.com/authorize?client_id={appId}&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=friends,wall&response_type=token&v=5.52");
+        }
+
+        private void GetToken_LoadCompleted(Object sender, EventArgs e)
+        {
+            if (GetToken.Source.ToString().Contains("access_token="))
+            {
+                GetUserToken();
+            }
+        }
+
+        private void GetUserToken()
+        {
+            var separators = new Char[]{ '=', '&' };
+            String[] split = GetToken.Source.ToString().Split(separators);
+            userToken = split[1];
+            userId = split[5];
+            this.Hide();
+        }
+
+        public String UserToken => userToken;
+        public String UserId => userId;
+    }
+}
---
+++ P_Likes/MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using Newtonsoft.Json.Linq;
+using System.Net;
+using System.IO;
+
+namespace P_Likes
+{
+    public partial class MainWindow : Window
+    {
+        private String userToken = String.Empty;
+        private String userId = String.Empty;
+        const String domain = "p_lazarus";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var window = new AuthWindow();
+            window.ShowDialog();
+
+            userToken = window.UserToken;
+            userId = window.UserId;
+            VkIdLabel.Content = $"Vk Id: {window.UserId}";
+            window.Close();
+
+            // если вход не осуществился
+            if (userToken == String.Empty)
+            {
+                this.Close();
+            }
+        }
+
+        private void LikeButton_Click(Object sender, RoutedEventArgs e)
+        {
+            LikePostsAsync();
+        }
+
+        private async void LikePostsAsync()
+        {
+            if (!Int32.TryParse(PostsNumberBox.Text, out Int32 postsNumber))
+            {
+                MessageBox.Show("Number parsing error.");
+                PostsNumberBox.Text = String.Empty;
+                return;
+            }
+
+            var queryUri = $@"https://api.vk.com/method/wall.get?v=5.52&domain={domain}&count={postsNumber}&extended=1&access_token={userToken}";
+
+            // отправка запроса на сервер и получение объекта ответа
+            var request = WebRequest.Create(queryUri);
+            JObject jsonData = null;
+            using (var response = request.GetResponse())
+            {
+                // оправить запрос на получение постов на П в формате JSON
+                jsonData = JObject.Parse(new StreamReader(response.GetResponseStream()).ReadToEnd());
+            }
+
+            // пропарсить JSON и получить все посты на П
+            var result =
+                from post in jsonData["response"]["items"]
+                let id = (Int32)post["id"]
+                let owner = (Int32)post["owner_id"]
+                select new
+                {
+                    Id = id,
+                    Owner = owner,
+                };
+
+            ProgressBar.Maximum = postsNumber;
+            ProgressBar.Value = 0;
+            LikeButton.IsEnabled = false;
+
+            foreach (var post in result)
+            {
+                String likeQueryUri = $@"https://api.vk.com/method/likes.add?v=5.52&type=post&owner_id={post.Owner}&item_id={post.Id}&access_token={userToken}";
+                request = WebRequest.Create(likeQueryUri) as HttpWebRequest;
+
+                // отправить запрос на лайк   
+                using (var resp = await request.GetResponseAsync()) { }
+                ProgressBar.Value++;
+            }
+
+            this.Close();
+        }
+    }
+}
